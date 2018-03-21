5bb31eabdc076a4cf1c4bbd7ed70633f996a6069
Luiz <luizrodrigo46@hotmail.com>
2018-03-21 10:31:28 -0300

    field 'adoted' renamed to 'is_adopted' on Pet model

--- backend/pet/models.py
+++ backend/pet/models.py
@@ -18,7 +18,7 @@ class Pet(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=50)
     age = models.DateField()
-    adoted = models.BooleanField()
+    is_adopted = models.BooleanField()
     race = models.CharField(max_length=50)
     pelage = models.CharField(max_length=50)
 
