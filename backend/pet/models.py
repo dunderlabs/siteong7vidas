d6d238c86250c3950fb81c43e695864d4a335394
Luiz <luizrodrigo46@hotmail.com>
2018-03-21 10:45:50 -0300

    field _type added to Pet model

--- backend/pet/models.py
+++ backend/pet/models.py
@@ -15,12 +15,21 @@ class PetBreed(models.Model):
 
 class Pet(models.Model):
 
+    PET_TYPE_CHOICES = (
+        ('cat', 'cat'),
+        ('dog', 'dog'),
+    )
+
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=50)
     age = models.DateField()
     is_adopted = models.BooleanField()
     breed = models.ForeignKey(PetBreed, on_delete=models.CASCADE)
     pelage = models.CharField(max_length=50)
+    _type = models.CharField(
+        max_length=10,
+        choices=PET_TYPE_CHOICES
+    )
 
     class Meta:
         verbose_name = "Pet"
